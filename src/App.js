535aa23751fcac40b90ad08d7174cae2ae782fff
simonemarketti <simomarketti@icloud.com>
2022-09-07 00:08:51 +0200

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useEffect, useState, useCallback} from "react";
 import ScoreBoard from "./Components/Scoreboard";
 
 import blueCandy from "./images/blue-candy.png"
@@ -10,6 +10,7 @@ import yellowCandy from "./images/yellow-candy.png"
 import blank from "./images/blank.png"
 
 const width = 8
+
 const candyColors = [
     blueCandy,
     greenCandy,
@@ -22,28 +23,37 @@ const candyColors = [
 function App() {
     const [currentColorArrangement, setCurrentColorArrangement] = useState([])
     const [squareBeingDragged, setSquareBeingDragged] = useState(null)
-    const [squareBeingPlaced, setSquareBeingPlaced] = useState(null)
+    const [squareBeingReplaced, setSquareBeingReplaced] = useState(null)
     const [scoreDisplay, setScoreDisplay] = useState(0)
 
 
-    const checkForRowsOfFour = () => {
+    // ----- FUNCTIONS TO CHECK ROWS AND COLUMNS -----
+
+    const checkForRowsOfFour = useCallback(() => {
+
+        // For every square in the gameplay
         for (let i = 0; i < 64; i++) {
             const rowOfFour = [i, i + 1, i + 2, i + 3]
             const decidedColor = currentColorArrangement[i]
             const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
             const isBlank = currentColorArrangement[i] === blank
 
+            // Don't watch for row match if is one element is on next line
             if (notValid.includes(i)) continue
 
+            // If every square in the array is the same color (decidedColor) and none is blank
             if (rowOfFour.every(square => currentColorArrangement[square] === decidedColor && !isBlank)) {
+                // Set every square blank
                 rowOfFour.forEach(square => currentColorArrangement[square] = blank)
+                // Increment the score
                 setScoreDisplay((prevState) => prevState + 4)
+                // Need it in dragend function below
                 return true
             }
         }
-    }
+    }, [currentColorArrangement])
 
-    const checkForColumnsOfFour = () => {
+    const checkForColumnsOfFour = useCallback(() => {
         for (let i = 0; i < 39; i++) {
             const columnOfFour = [i, i + width, i + width * 2, i + width * 3]
             const decidedColor = currentColorArrangement[i]
@@ -55,9 +65,9 @@ function App() {
                 return true
             }
         }
-    }
+    }, [currentColorArrangement])
 
-    const checkForColumnsOfThree = () => {
+    const checkForColumnOfThree = useCallback(() => {
         for (let i = 0; i < 47; i++) {
             const columnOfThree = [i, i + width, i + width * 2]
             const decidedColor = currentColorArrangement[i]
@@ -69,9 +79,9 @@ function App() {
                 return true
             }
         }
-    }
+    }, [currentColorArrangement])
 
-    const checkForRowsOfThree = () => {
+    const checkForRowsOfThree = useCallback(() => {
         for (let i = 0; i < 64; i++) {
             const rowOfThree = [i, i + 1, i + 2]
             const decidedColor = currentColorArrangement[i]
@@ -86,64 +96,86 @@ function App() {
                 return true
             }
         }
-    }
+    }, [currentColorArrangement])
 
-    const moveIntoSquareBelow = () => {
+
+    // ----- FUNCTION TO HANDLE WHITESPACES -----
+
+    const moveIntoSquareBelow = useCallback(() => {
         for (let i = 0; i <= 55; i++) {
+            // Select elements in first row (top)
             const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
             const isFirstRow = firstRow.includes(i)
 
+            // If an element is in first row (top) and is blank
             if (isFirstRow && currentColorArrangement[i] === blank) {
+                // Select a random color
                 let randomNumber = Math.floor(Math.random() * candyColors.length)
+                // Assign new random color to blank square
                 currentColorArrangement[i] = candyColors[randomNumber]
             }
 
+            /* If the same square in column below is blank too, assign the value to this and turn the upper square blank
+            (change in dependencies cause the rerender) */
             if (currentColorArrangement[i + width] === blank) {
                 currentColorArrangement[i + width] = currentColorArrangement[i]
                 currentColorArrangement[i] = blank
             }
         }
-    }
+    }, [currentColorArrangement])
+
+    // ----- FUNCTIONS TO MOVE CANDIES -----
 
     const dragStart = (e) => {
         setSquareBeingDragged(e.target)
     }
 
     const dragDrop = (e) => {
-        setSquareBeingPlaced(e.target)
+        setSquareBeingReplaced(e.target)
     }
 
     const dragEnd = () => {
         const squareBeingDraggedId = parseInt(squareBeingDragged.getAttribute('data-id'))
-        const squareBeingReplacedId = parseInt(squareBeingPlaced.getAttribute('data-id'))
+        const squareBeingReplacedId = parseInt(squareBeingReplaced.getAttribute('data-id'))
 
         const validMoves = [
+            // Move in rows
             squareBeingDraggedId - 1,
-            squareBeingDraggedId - width,
             squareBeingDraggedId + 1,
+            // Move in columns
+            squareBeingDraggedId - width,
             squareBeingDraggedId + width
         ]
 
         const validMove = validMoves.includes(squareBeingReplacedId)
 
         if (validMove) {
+            // Invert images with source attribute
             currentColorArrangement[squareBeingReplacedId] = squareBeingDragged.getAttribute('src')
-            currentColorArrangement[squareBeingDraggedId] = squareBeingPlaced.getAttribute('src')
+            currentColorArrangement[squareBeingDraggedId] = squareBeingReplaced.getAttribute('src')
+
+            // If one of these functions return true (see above)
             const isAColumnOfFour = checkForColumnsOfFour()
             const isARowOfFour = checkForRowsOfFour()
-            const isAColumnOfThree = checkForColumnsOfThree()
+            const isAColumnOfThree = checkForColumnOfThree()
             const isARowOfThree = checkForRowsOfThree()
+
+            // Reset the values after the candies movements
             if (squareBeingReplacedId &&
                 (isARowOfThree || isARowOfFour || isAColumnOfFour || isAColumnOfThree)) {
                 setSquareBeingDragged(null)
-                setSquareBeingPlaced(null)
+                setSquareBeingReplaced(null)
+
+            // Otherwise leave the candies in the same places
             } else {
-                currentColorArrangement[squareBeingReplacedId] = squareBeingPlaced.getAttribute('src')
+                currentColorArrangement[squareBeingReplacedId] = squareBeingReplaced.getAttribute('src')
                 currentColorArrangement[squareBeingDraggedId] = squareBeingDragged.getAttribute('src')
             }
         }
-
     }
+
+
+    // ----- CREATE THE GAME -----
 
     const createBoard = () => {
         const randomColorArrangement = []
@@ -158,19 +190,20 @@ function App() {
         createBoard()
     }, [])
 
+    // ----- GAMEPLAY -----
 
     useEffect(() => {
 
         const timer = setInterval(() => {
             checkForColumnsOfFour()
             checkForRowsOfFour()
-            checkForColumnsOfThree()
+            checkForColumnOfThree()
             checkForRowsOfThree()
             moveIntoSquareBelow()
             setCurrentColorArrangement([...currentColorArrangement])
         }, 100)
         return () => clearInterval(timer)
-    }, [checkForColumnsOfFour, checkForRowsOfFour, checkForColumnsOfThree, checkForRowsOfThree, moveIntoSquareBelow, currentColorArrangement])
+    }, [checkForColumnsOfFour, checkForRowsOfFour, checkForColumnOfThree, checkForRowsOfThree, moveIntoSquareBelow, currentColorArrangement])
 
 
     return (
---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBzsJDlNxIOxYSKP273a3LgM5WJlCP4RyE",
+    authDomain: "candy-crush-react-f4f88.firebaseapp.com",
+    projectId: "candy-crush-react-f4f88",
+    storageBucket: "candy-crush-react-f4f88.appspot.com",
+    messagingSenderId: "328581520621",
+    appId: "1:328581520621:web:27505261744efc85c500e4"
+};
+
+//init firebase
+initializeApp(firebaseConfig)
